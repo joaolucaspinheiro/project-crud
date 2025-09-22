be74368f6f4989f8da369966296aba523f515730
João Lucas Pinheiro <lucasdallas25@gmail.com>
2025-09-21 22:31:57 -0300

    feat: enhance CreateMessageDto with validation and add global validation pipe

--- src/messages/dto/create-message.dto.ts
+++ src/messages/dto/create-message.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateMessageDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(255)
   readonly text: string;
+  @IsString()
+  @IsNotEmpty()
   readonly from: string;
+  @IsString()
+  @IsNotEmpty()
   readonly to: string;
 }
