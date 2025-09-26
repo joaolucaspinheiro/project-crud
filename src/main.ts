735314bd17a5a68194e38a175794a6052562573b
João Lucas Pinheiro <lucasdallas25@gmail.com>
2025-09-25 22:33:02 -0300

    feat: enhance global validation pipe with whitelist and forbidNonWhitelisted options

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remove properties that do not have any decorators
+      forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are present
+      transform: false, // Automatically transform payloads to be objects typed according to their DTO classes
+    }),
+  );
 }
 bootstrap();
